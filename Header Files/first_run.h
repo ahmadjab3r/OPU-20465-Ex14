078015ee4bd1830b8e711a6d53dff44cfd3b1a8e
Ahmad Jaber <39513952+ahmadjab3r@users.noreply.github.com>
2025-10-14 01:47:07 +0300

    Restore the file Header Files/first_run.h

---
+++ Header Files/first_run.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "skeleton.h"
+#include "utilities.h"
+void first_run(  as_file* current_as_file,constants *globals);
+int handle_line(as_file * current_as_file, char * str,int with_label) ;
